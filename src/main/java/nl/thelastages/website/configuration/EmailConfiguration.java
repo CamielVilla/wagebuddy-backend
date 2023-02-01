6dd4ffd3747dd686e2fadbed6af053d6d2daf4fe
CamielVilla <camielvilla@gmail.com>
2023-02-01 12:36:13 +0100

    email succeed

--- src/main/java/nl/thelastages/website/configuration/EmailConfiguration.java
+++ src/main/java/nl/thelastages/website/configuration/EmailConfiguration.java
@@ -1,16 +1,35 @@
 package nl.thelastages.website.configuration;
 
+import com.fasterxml.jackson.databind.annotation.JsonAppend;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 
+import java.io.InputStream;
+import java.util.Date;
 import java.util.Properties;
-
-//@PropertySource(value={"classpath:application.yml"})
+@Configuration
+@PropertySource("classpath:application.properties")
 public class EmailConfiguration {
+
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        return new JavaMailSenderImpl();
+    }
+}
+
 //    @Value("${spring.mail.host}")
 //    private String mailServerHost;
 //
@@ -32,30 +51,26 @@ public class EmailConfiguration {
 //    @Value("${spring.mail.templates.path}")
 //    private String mailTemplatesPath;
 
-    @Bean
-    public JavaMailSender getJavaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
 
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
+//        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
 
-        mailSender.setUsername("camielvilla@gmail.com");
-        mailSender.setPassword("pp44ucemyoYBwa");
+//        mailSender.setHost("smtp.gmail.com");
+//        mailSender.setPort(587);
+//
 
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
-
-        return mailSender;
-    }
-
-    @Bean
-    public SimpleMailMessage templateSimpleMessage() {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setText("This is the test email template for your email:\n%s\n");
-        return message;
-    }
-}
+//
+//        Properties props = mailSender.getJavaMailProperties();
+//        props.put("mail.transport.protocol", "smtp");
+//        props.put("mail.smtp.auth", "true");
+//        props.put("mail.smtp.starttls.enable", "true");
+//        props.put("mail.debug", "true");
+//
+//}
+//
+//    @Bean
+//    public SimpleMailMessage templateSimpleMessage() {
+//        SimpleMailMessage message = new SimpleMailMessage();
+//        message.setText("This is the test email template for your email:\n%s\n");
+//        return message;
+//    }
 
--- src/main/java/nl/thelastages/website/service/UserService.java
+++ src/main/java/nl/thelastages/website/service/UserService.java
@@ -1,35 +1,71 @@
 package nl.thelastages.website.service;
 
+import jakarta.mail.*;
+import jakarta.mail.internet.MimeMessage;
 import nl.thelastages.website.model.dto.CreateUserDto;
 import nl.thelastages.website.model.dto.UserDto;
 import nl.thelastages.website.model.entity.User;
 import nl.thelastages.website.respository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.Properties;
 
 @Service
-@Transactional
 public class UserService implements IUserService{
-    @Autowired
     UserRepository userRepository;
 
+    private String userName;
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public Boolean addEmail(CreateUserDto dto) {
         Optional<User> optionalEmail = userRepository.findEmailByEmail(dto.getEmailAddress());
         if (!optionalEmail.isPresent()) {
             User user = new User();
             user.setEmail(dto.getEmailAddress());
+            JavaMailSenderImpl sender = new JavaMailSenderImpl();
+            Properties props = new Properties();
+            props.setProperty("mail.transport.protocol", "smtp");
+            props.setProperty("mail.host", "smtp.gmail.com");
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.port", "587");
+            props.put("mail.smtp.socketFactory.port", "465");
+            props.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");
+            props.put("mail.smtp.socketFactory.fallback", "false");
+            props.put("mail.username", "camielvilla@gmail.com");
+            props.put("mail.password", "");
+            sender.setJavaMailProperties(props);
+            Session session = Session.getInstance(props, new Authenticator() {
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication("camielvilla@gmail.com", "brvloeuswhwfwqkl");
+                }
+            });
+            try{
+                MimeMessage msg = new MimeMessage(session);
+                msg.setFrom();
+                msg.setRecipients(Message.RecipientType.TO,
+                        "camielvilla@gmail.com");
+                msg.setSubject("JavaMail hello world example3");
+                msg.setSentDate(new Date());
+                msg.setText("Hello, world!\n");
+                Transport.send(msg);
+            }catch (MessagingException mex){
+                System.out.println("send failed, exception: " + mex);
+            }
             userRepository.save(user);
             return true;
         }else {
             return false;
         }
     }
-
 
 
 
