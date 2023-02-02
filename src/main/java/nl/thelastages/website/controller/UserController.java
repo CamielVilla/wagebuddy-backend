fefd81fe37e99e6c8d61754b6618d32f78fedbd7
CamielVilla <camielvilla@gmail.com>
2023-02-02 16:29:02 +0100

    edited conformation email

--- src/main/java/nl/thelastages/website/controller/UserController.java
+++ src/main/java/nl/thelastages/website/controller/UserController.java
@@ -1,19 +1,12 @@
 package nl.thelastages.website.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
-import nl.thelastages.website.OnRegistrationCompleteEvent;
-import nl.thelastages.website.exception.UserAlreadyExistException;
-import nl.thelastages.website.model.dto.UserDto;
-import nl.thelastages.website.model.entity.User;
 import nl.thelastages.website.service.UserService;
 import nl.thelastages.website.model.dto.CreateUserDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 //@RequestMapping("thelastages")
